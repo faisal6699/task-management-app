94b76fb164dcfa7d7d451756906a2069f15358f5
faisal6699 <faisal.ahmed6699@gmail.com>
2022-07-28 22:33:00 +0600

    initiate project

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {makeServer} from "./server";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// initiate fake server;
+makeServer();
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
